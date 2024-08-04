0c49c74e296351bba419f83659232e901095da4c
Jonah Phillips <151664990+jonahp-coding@users.noreply.github.com>
2024-08-04 16:56:13 -0500

    accelerate.cpp

--- accelerate.cpp
+++ accelerate.cpp
@@ -1,7 +1,8 @@
 void accelerate_motor(uint8_t target_speed_param, uint16_t rate_param, uint8_t initial_speed_param) {
   uint8_t target_speed = target_speed_param; // { adjust this value as you please... }
-  uint16_t rate = rate_param; // how long motor should take to reach 'R_target_speed' in 'ms' { adjust this value as you please... }
   uint8_t initial_speed = initial_speed_param; // the intial 'pwm' speed of motor at beginning of acceleration { adjust this value as you please... }
+  uint16_t rate = rate_param; // how long motor should take to reach 'R_target_speed' in 'ms' { adjust this value as you please... }
+  uint16_t current_millis = millis();
   
   uint8_t speed; // instantaneous speed of motor
   uint16_t acceleration_timestamp; // instantaneous timestamp of motor acceleration
@@ -16,7 +17,7 @@ void accelerate_motor(uint8_t target_speed_param, uint16_t rate_param, uint8_t i
     speed = map(current_millis, initial_timestamp, (initial_timestamp + rate), initial_speed, target_speed);
     acceleration_timestamp = current_millis - initial_timestamp;
   
-    motor_set_pwm(speed);
+    motor_set_pwm(pin, speed);
   
     Serial.print("speed(pwm): ");
     Serial.println(speed);
@@ -24,4 +25,8 @@ void accelerate_motor(uint8_t target_speed_param, uint16_t rate_param, uint8_t i
     Serial.println(acceleration_timestamp);
     Serial.println("-----------------------------");
   }
+}
+
+void motor_set_pwm(uint8_t motor_pin, uint8_t pwm_val) {
+  analogWrite(motor_pin, pwm_val);
 }
