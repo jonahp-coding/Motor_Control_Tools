ed28f395a009a3400325d13928ed50d255ef388f
Jonah Phillips <151664990+jonahp-coding@users.noreply.github.com>
2024-08-04 14:32:00 -0500

    accelerate.cpp

--- accelerate.cpp
+++ accelerate.cpp
@@ -1,26 +1,24 @@
-uint8_t R_target_speed = 150; // { adjust this value as you please... }
-uint16_t R_rate = 5000; // how long motor 'R' should take to reach 'R_target_speed' { adjust this value as you please... }
-uint8_t R_speed; // instantaneous speed of motor 'R'
-uint16_t R_acceleration_timestamp; // instantaneous timestamp of motor 'R' acceleration
-uint16_t R_initial_timestamp; // timestamp at beginning of motor 'R' acceleration
-uint8_t R_initial_speed = 50; // the intial speed of motor 'L' at beginning of acceleration { adjust this value as you please... }
+uint8_t target_speed = 150; // { adjust this value as you please... }
+uint16_t rate = 5000; // how long motor should take to reach 'R_target_speed' { adjust this value as you please... }
+uint8_t speed; // instantaneous speed of motor
+uint16_t acceleration_timestamp; // instantaneous timestamp of motor acceleration
+uint16_t initial_timestamp; // timestamp at beginning of motor acceleration
+uint8_t initial_speed = 50; // the intial speed of motor at beginning of acceleration { adjust this value as you please... }
 
-void acceleration_test(uint16_t rate) {
-  motor_1_set_pwm(R_initial_speed);
+motor_set_pwm(initial_speed);
+current_millis = millis();
+initial_timestamp = current_millis;
+
+while (current_millis - initial_timestamp < rate) {
   current_millis = millis();
-  R_initial_timestamp = current_millis;
+  speed = map(current_millis, initial_timestamp, (initial_timestamp + rate), initial_speed, target_speed);
+  acceleration_timestamp = current_millis - initial_timestamp;
 
-  while (current_millis - R_initial_timestamp < rate) {
-    current_millis = millis();
-    R_speed = map(current_millis, R_initial_timestamp, (R_initial_timestamp + rate), R_initial_speed, R_target_speed);
-    R_acceleration_timestamp = current_millis - R_initial_timestamp;
+  motor_set_pwm(speed);
 
-    motor_1_set_pwm(R_speed);
-
-    Serial.print("R_speed: ");
-    Serial.println(R_speed);
-    Serial.print("R_acceleration_timestamp(ms): ");
-    Serial.println(R_acceleration_timestamp);
-    Serial.println("-----------------------------");
-  }
+  Serial.print("speed: ");
+  Serial.println(speed);
+  Serial.print("acceleration_timestamp(ms): ");
+  Serial.println(acceleration_timestamp);
+  Serial.println("-----------------------------");
 }
